68603946997b59974c3024cce9294213c79eaeba
Rogelio Guzman <chinog9@gmail.com>
2012-11-01 15:20:44 -0600

    [FEATURE] Showing time and sharewall latest links

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -1,17 +1,22 @@
-require 'net/http'
+require 'rest_client'
 require 'json'
 
-search_term = URI::encode('#todayilearned')
 
-SCHEDULER.every '10m', :first_in => 0 do |job|
-  http = Net::HTTP.new('search.twitter.com')
-  response = http.request(Net::HTTP::Get.new("/search.json?q=#{search_term}"))
-  tweets = JSON.parse(response.body)["results"]
-  if tweets
-    tweets.map! do |tweet| 
-      { name: tweet['from_user'], body: tweet['text'], avatar: tweet['profile_image_url_https'] }
+url = 'http://sharewall.herokuapp.com/links.json'
+
+SCHEDULER.every '5m', :first_in => 0 do |job|
+  response = RestClient.get url
+  links = JSON.parse(response).reverse[0..5]
+
+  if links
+    links.map! do |link| 
+      { 
+        body: link['title'], 
+        name: link['user']['name'], 
+        avatar: link['user']['image_url'] 
+      }
     end
   
-    send_event('twitter_mentions', comments: tweets)
+    send_event('twitter_mentions', comments: links)
   end
 end
