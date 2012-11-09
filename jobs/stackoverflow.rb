272f35aab8676b1016ec5bdbe69a8da6d9809174
e3matheus <e3matheus@gmail.com>
2012-11-09 10:56:54 -0600

    Added stackoverflow reputation widget

---
+++ jobs/stackoverflow.rb
@@ -0,0 +1,14 @@
+user_hash = {
+  800254 => 'rogeliog',
+  103076 => 'gajon',
+  314346 => 'macario',
+  562692 => 'e3matheus',
+  803847 => 'marrossa',
+  803844 => 'adriancuadros'
+}
+
+SCHEDULER.every '5m', :first_in => 0 do |job|
+  client = StackoverflowClient.new user_hash
+  client.execute
+  send_event('stackoverflow_reputation', items: client.data)
+end
